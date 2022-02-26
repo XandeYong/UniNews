247d50eac41fc0546192fab2ce076c8866da035f
Adrian Fong <adrain2000@live.com>
2022-02-27 01:09:54 +0800

    Test Commit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                         <div class="row w-100">
 
                             <div id="nav-title" class="col-12 col-sm-5 col-md-6">
-                                <a href="./index.php">
+                                <a href="./news.php">
                                     <h3 class="d-flex">
                                         <span>Uni</span>
                                         <span>News</span>
