8485dd0cc3cf8579c996bcf19a9456f0688ffbd4
Adrian Fong <adrain2000@live.com>
2022-03-03 17:25:48 +0800

    Main prupose of account function done, dashboard able to logged with the needed data, created temp new Account and News class without referring UML diagram

--- backend/login.php
+++ backend/login.php
@@ -1,10 +1,62 @@
 <?php 
+    require_once('../model/Account.php');
 
-    header('Location: ../dashboard/dashboard_index.php');
+    //Main part [Verification process]
+    if(isset($_POST["username"]) && isset($_POST["password"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        //Validate
+        if(executeQuery(dbConnection(), $username, $password)){
+            header('Location: ../dashboard/dashboard_index.php');
+        }else{
+            $_SESSION["login_error"] = "Invalid login credentials!";
+            header('Location: ../admin_login.php');
+        }
+    }
 
 
+    //Establishing connection
+    function dbConnection(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "uninews";
 
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);  
 
+        //Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }else{
+            return $conn;
+        }
+          
+    }
+    
+    //Query execution module
+    function executeQuery($conn, $username, $password){
 
+        //Query
+        $sql = "SELECT * FROM account WHERE username = '$username' AND password = '$password'";
+
+        //Running Query
+        $result = $conn->query($sql);
+
+        //Creating class based on result
+        if($result->num_rows > 0){
+            $row = $result -> fetch_assoc();
+
+            // Start the session
+            session_start();
+
+            $loginAccount = new Account($row["account_id"], $row["username"], $row["email"], $row["password"]); 
+            $_SESSION["account"] = serialize($loginAccount);             
+            return true;        
+        }else{
+            return false;
+        }
+    }   
 
 ?>
---
+++ model/News.php
@@ -0,0 +1,90 @@
+<?php
+class News{
+    //Variables
+    private $news_id;
+    private $title;
+    private $description;
+    private $image;
+    private $status;
+    private $category;
+    private $subCategory;
+    private $comments;
+
+    //Constructors
+    function __construct($news_id, $title, $description, $image, $status, $category, $subCategory){
+        $this->news_id = $news_id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->image = $image;
+        $this->status = $status;
+        $this->category = $category;
+        $this->subCategory = $subCategory;
+        $this->comments = array();
+    }
+
+    //Getter & Setter
+    function get_news_id() {
+        return $this->news_id;
+    }
+
+    function set_news_id($news_id){
+        $this->news_id = $news_id;
+    }
+
+    function get_title() {
+        return $this->title;
+    }
+
+    function set_title($title){
+        $this->title = $title;
+    }
+
+    function get_description() {
+        return $this->description;
+    }
+
+    function set_description($description){
+        $this->description = $description;
+    }
+
+    function get_image() {
+        return $this->image;
+    }
+
+    function set_image($image){
+        $this->image = $image;
+    }
+
+    function get_status() {
+        return $this->status;
+    }
+
+    function set_status($status){
+        $this->status = $status;
+    }
+
+    function get_category() {
+        return $this->category;
+    }
+
+    function set_category($category){
+        $this->category = $category;
+    }
+
+    function get_subCategory() {
+        return $this->subCategory;
+    }
+
+    function set_subCategory($subCategory){
+        $this->subCategory = $subCategory;
+    }
+
+    function get_comments() {
+        return $this->comments;
+    }
+
+    function set_comments($comments){
+        $this->comments = $comments;
+    }
+}
+?>
